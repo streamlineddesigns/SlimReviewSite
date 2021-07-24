a1cce9d44d123859d6d164f065ba2e6de45681a1
streamlineddesigns <streamline.our.designs@gmail.com>
2021-07-23 23:55:52 -0400

    Added copy to clipboard functionality

--- public/scripts/generate.js
+++ public/scripts/generate.js
@@ -14,6 +14,7 @@ function GenerateReplyApp() {
         this.categoryChangeListener();
         this.generateReplyButtonClickListener();
         this.generateNextReplyButtonClickListener();
+        this.copyToClipboardClickListener();
 
         if (GenerateReplyApp.instance == null) {
             GenerateReplyApp.instance = this;
@@ -126,5 +127,65 @@ function GenerateReplyApp() {
         $("#generate-reply-button").hide();
         $("#confirm-generated-reply-button").show();
         $("#generate-next-reply-button").show();
+    }
+
+    /* copy to clip board click listener */
+    this.copyToClipboardClickListener = function() {
+        document.getElementById("copy-to-clipboard-button").addEventListener("click", function() {
+            if (GenerateReplyApp.instance.copyToClipboard(document.getElementById("textarea_reply_text"))) {
+                swal("Success!", "The reply text was copied to your clipboard!", "success");
+            }
+        });
+    }
+
+    /* Copies reply to clipboard */
+    this.copyToClipboard = function(elem) {
+        // create hidden text element, if it doesn't already exist
+        var targetId = "_hiddenCopyText_";
+        var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+        var origSelectionStart, origSelectionEnd;
+        if (isInput) {
+            // can just use the original source element for the selection and copy
+            target = elem;
+            origSelectionStart = elem.selectionStart;
+            origSelectionEnd = elem.selectionEnd;
+        } else {
+            // must use a temporary form element for the selection and copy
+            target = document.getElementById(targetId);
+            if (!target) {
+                var target = document.createElement("textarea");
+                target.style.position = "absolute";
+                target.style.left = "-9999px";
+                target.style.top = "0";
+                target.id = targetId;
+                document.body.appendChild(target);
+            }
+            target.textContent = elem.textContent;
+        }
+        // select the content
+        var currentFocus = document.activeElement;
+        target.focus();
+        target.setSelectionRange(0, target.value.length);
+        
+        // copy the selection
+        var succeed;
+        try {
+                succeed = document.execCommand("copy");
+        } catch(e) {
+            succeed = false;
+        }
+        // restore original focus
+        if (currentFocus && typeof currentFocus.focus === "function") {
+            currentFocus.focus();
+        }
+        
+        if (isInput) {
+            // restore prior selection
+            elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+        } else {
+            // clear temporary content
+            target.textContent = "";
+        }
+        return succeed;
     }
 }
